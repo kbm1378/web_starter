c04611b9bcb9f17aa4a6c6c4c814bd29bdaf4f27
Byungmook Kim/김병묵 <47764242+vuno-bmkim@users.noreply.github.com>
2020-04-24 18:22:41 +0900

    Add Upload starter

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from datetime import datetime
+import os
+
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
@@ -9,6 +15,10 @@ db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 
 @app.route('/order', methods=['POST'])
@@ -31,6 +41,19 @@ def post_order():
 def get_order():
     orders = list(db.orders.find({}, {'_id': 0}))
     return jsonify({'result': 'success', 'orders': orders})
+
+@app.route('/api/upload', methods=['POST'])
+def post_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            now = datetime.now()
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
+            file.save(filename)
+            return jsonify({"success":True})
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
